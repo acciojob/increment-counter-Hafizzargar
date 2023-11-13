aa22b52fc5217a08b38cf6b565893bcf03ee62b0
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-11-14 00:50:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 //your JS code here. If required.
 let x=1;
 let co=document.getElementById("counter");
+var counterValue = 0;
+ var incrementBtn = document.getElementById('incrementBtn');
+ incrementBtn.addEventListener('click', function() {
+            
+            alert(counterValue);
+
+            
+            counterValue++;
+	 co.innerText=counterValue;
+
+            
+            
+        });
 
 
-function f1(){
-	alert(co.innerText);
-	co.innerText=x++;
+// function f1(){
+// 	alert(co.innerText);
+// 	co.innerText=x++;
 	
-}
+// }
 
